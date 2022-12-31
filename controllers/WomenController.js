0f7f0d08e1361df860e95a749dfe2df3fba967c7
stephenwayar <stephenbuluswayar@gmail.com>
2022-12-31 21:11:25 +0100

    update: made changes to womens controller to have image upload

--- controllers/WomenController.js
+++ controllers/WomenController.js
@@ -1,15 +1,21 @@
 const Product = require('../models/Product')
 const logger = require('../utils/logger')
+const uploadProductImg = require('../helpers/uploadProductImg')
 
 //clothing controllers
-exports.get_women_clothes = (req, res, next) => {
-  let TAG = 'women_clothe'
-  Product.find({ tag: TAG }).then(products => {
+exports.get_women_clothes = async (req, res, next) => {
+  const tag = 'women_clothe'
+
+  try{
+    const products = await Product.find({ tag })
+
     res.status(200).json(products)
-  }).catch(error => next(error))
+  }catch(error){
+    next(error)
+  }
 }
 
-exports.add_women_clothe = (req, res, next) => {
+exports.add_women_clothe = async (req, res, next) => {
   if (!req.user) {
     logger.info('token is missing')
     return res.status(401).json({
@@ -17,37 +23,57 @@ exports.add_women_clothe = (req, res, next) => {
     })
   }
 
-  let body = req.body
-  let TAG = 'women_clothe'
-  let product = new Product({
-    img_URL: body.img_URL,
-    name: body.name,
-    description: body.description,
-    price: body.price,
-    quantity: body.quantity,
-    tag: TAG
-  })
-  product.save().then(product => {
-    logger.info("Clothe saved!")
-    res.status(200).json(product)
-  }).catch(error => {
-    logger.error('Error! Clothe not saved', error)
+  const product = await Product.findOne({ name: req.body.name })
+
+  if(product) {
+    return res.status(400).json({
+      success: false,
+      message: `A product with the name '${req.body.name}' already exists`
+    })
+  }
+
+  const body = req.body
+  const images = body.images // array
+  const tag = 'women_clothe'
+
+  try{
+    const img = await uploadProductImg(images)
+
+    const product = new Product({
+      images: img,
+      name: body.name,
+      description: body.description,
+      price: body.price, // array of objects
+      tag
+    })
+
+    const savedProduct = await product.save()
+
+    res.status(200).json(savedProduct)
+  }catch(error){
+    logger.error('Failed to add women_clothe', error)
+
     res.status(400).json({
       success: false,
-      message: "Error! Clothe not saved"
+      message: 'There was an error adding product'
     })
-  })
+  }
 }
 
 // shoe controllers
-exports.get_women_shoes = (req, res, next) => {
-  let TAG = 'women_shoe'
-  Product.find({ tag: TAG }).then(products => {
+exports.get_women_shoes = async (req, res, next) => {
+  const tag = 'women_shoe'
+
+  try{
+    const products = await Product.find({ tag })
+
     res.status(200).json(products)
-  }).catch(error => next(error))
+  }catch(error){
+    next(error)
+  }
 }
 
-exports.add_women_shoe = (req, res, next) => {
+exports.add_women_shoe = async (req, res, next) => {
   if (!req.user) {
     logger.info('token is missing')
     return res.status(401).json({
@@ -55,37 +81,57 @@ exports.add_women_shoe = (req, res, next) => {
     })
   }
 
-  let body = req.body
-  let TAG = 'women_shoe'
-  let product = new Product({
-    img_URL: body.img_URL,
-    name: body.name,
-    description: body.description,
-    price: body.price,
-    quantity: body.quantity,
-    tag: TAG
-  })
-  product.save().then(product => {
-    logger.info('Shoe saved!')
-    res.status(200).json(product)
-  }).catch(error => {
-    logger.error('Error! Shoe not saved', error)
+  const product = await Product.findOne({ name: req.body.name })
+
+  if(product) {
+    return res.status(400).json({
+      success: false,
+      message: `A product with the name '${req.body.name}' already exists`
+    })
+  }
+
+  const body = req.body
+  const images = body.images //array
+  const tag = 'women_shoe'
+
+  try{
+    const img = await uploadProductImg(images)
+
+    const product = new Product({
+      images: img,
+      name: body.name,
+      description: body.description,
+      price: body.price, // array of objects
+      tag
+    })
+
+    const savedProduct = await product.save()
+
+    res.status(200).json(savedProduct)
+  }catch(error){
+    logger.error('Failed to add women_shoe', error)
+
     res.status(400).json({
       success: false,
-      message: "Error! Shoe not saved"
+      message: 'There was an error adding product'
     })
-  })
+  }
 }
 
 //accessories controllers
-exports.get_women_accessories = (req, res, next) => {
-  let TAG = 'women_accessory'
-  Product.find({ tag: TAG }).then(products => {
+exports.get_women_accessories = async (req, res, next) => {
+  const tag = 'women_accessory'
+
+  try{
+    const products = await Product.find({ tag })
+
     res.status(200).json(products)
-  }).catch(error => next(error))
+  }catch(error){
+    next(error)
+  }
 }
 
-exports.add_women_accessory = (req, res, next) => {
+exports.add_women_accessory = async (req, res, next) => {
   if (!req.user) {
     logger.info('token is missing')
     return res.status(401).json({
@@ -93,37 +139,57 @@ exports.add_women_accessory = (req, res, next) => {
     })
   }
 
-  let body = req.body
-  let TAG = 'women_accessory'
-  let product = new Product({
-    img_URL: body.img_URL,
-    name: body.name,
-    description: body.description,
-    price: body.price,
-    quantity: body.quantity,
-    tag: TAG
-  })
-  product.save().then(product => {
-    logger.info('Accessory saved!')
-    res.status(200).json(product)
-  }).catch(error => {
-    logger.error('Error! Accessory not saved', error)
+  const product = await Product.findOne({ name: req.body.name })
+
+  if(product) {
+    return res.status(400).json({
+      success: false,
+      message: `A product with the name '${req.body.name}' already exists`
+    })
+  }
+
+  const body = req.body
+  const images = body.images //array
+  const tag = 'women_accessory'
+
+  try{
+    const img = await uploadProductImg(images)
+
+    const product = new Product({
+      images: img,
+      name: body.name,
+      description: body.description,
+      price: body.price, // array of objects
+      tag
+    })
+
+    const savedProduct = await product.save()
+
+    res.status(200).json(savedProduct)
+  }catch(error){
+    logger.error('Failed to add women_accessory', error)
+
     res.status(400).json({
       success: false,
-      message: "Error! Accessory not saved"
+      message: 'There was an error adding product'
     })
-  })
+  }
 }
 
 //underwears controllers
-exports.get_women_underwears = (req, res, next) => {
-  let TAG = 'women_underwear'
-  Product.find({ tag: TAG }).then(products => {
+exports.get_women_underwears = async (req, res, next) => {
+  const tag = 'women_underwear'
+
+  try{
+    const products = await Product.find({ tag })
+
     res.status(200).json(products)
-  }).catch(error => next(error))
+  }catch(error){
+    next(error)
+  }
 }
 
-exports.add_women_underwear = (req, res, next) => {
+exports.add_women_underwear = async (req, res, next) => {
   if (!req.user) {
     logger.info('token is missing')
     return res.status(401).json({
@@ -131,24 +197,39 @@ exports.add_women_underwear = (req, res, next) => {
     })
   }
 
-  let body = req.body
-  let TAG = 'women_underwear'
-  let product = new Product({
-    img_URL: body.img_URL,
-    name: body.name,
-    description: body.description,
-    price: body.price,
-    quantity: body.quantity,
-    tag: TAG
-  })
-  product.save().then(product => {
-    logger.info('Underwear saved!')
-    res.status(200).json(product)
-  }).catch(error => {
-    logger.error('Error! Underwear not saved', error)
+  const product = await Product.findOne({ name: req.body.name })
+
+  if(product) {
+    return res.status(400).json({
+      success: false,
+      message: `A product with the name '${req.body.name}' already exists`
+    })
+  }
+
+  const body = req.body
+  const images = body.images //array
+  const tag = 'women_underwear'
+
+  try{
+    const img = await uploadProductImg(images)
+
+    const product = new Product({
+      images: img,
+      name: body.name,
+      description: body.description,
+      price: body.price, // array of objects
+      tag
+    })
+
+    const savedProduct = await product.save()
+
+    res.status(200).json(savedProduct)
+  }catch(error){
+    logger.error('Failed to add women_underwear', error)
+
     res.status(400).json({
       success: false,
-      message: "Error! Underwear not saved"
+      message: 'There was an error adding product'
     })
-  })
+  }
 }
